3ef0e578dd1b9ae59e1a875c38f072dc717ff258
hunter <hskempton@gmail.com>
2017-04-19 21:17:14 -0400

    added function to populate list, add total, add new items, and delete previous items

--- script.js
+++ script.js
@@ -7,24 +7,68 @@ var mayo = {name: "mayo", price: 4.49};
 var cheese = {name: "cheese", price: 6.99};
 var dogFood = {name: "dog food", price: 29.99};
 var beer = {name: "beer", price: 7.99};
-var pickles = {name: "cookies", price: 2.99}
+var pickles = {name: "pickles", price: 2.49}
 
 // create array using push method
 var list = [];
-list.push(onions,cookies,cucumburs,cornedBeef,potatoChips,pickles,mayo,cheese,dogFood,beer);
+list.push(onions,cookies,cucumburs,cornedBeef,potatoChips,mayo,cheese,dogFood,beer,pickles);
 
-// intialize total variable
-var total = 0;
+// create new container that is a div where we will put our grocery list
+var container = document.createElement('div');
+container.className = 'li';
+container.id ="listInDiv";
+document.getElementById('shoppingList').appendChild(container);
 
-// loop through list array printing name and price and adding to total
-list.forEach(function (i) {
-  console.log(i['name'] + " cost $" + i['price']);
-  total += i['price'];
-});
+// function that populates that list
+function populate () {
+  for (var i = 0; i < list.length ; i++) {
+    var newLine = document.createElement('li');
+    newLine.innerText = list[i].name + " cost $" + list[i].price;
+    container.appendChild(newLine);
+  }
+}
 
-// add tax to total
-total = total * 1.06;
-// round to 2 decimal places
-total = total.toFixed(2);
+// initial call
+populate();
 
-console.log('Your total is $' + total)
+// create object from form inputs and adds to li
+function addItem () {
+  var item = document.getElementById("item").value;
+  var priceOf = Number (document.getElementById("price").value);
+  var newObj = {name: item, price: priceOf};
+  list.push(newObj);
+  var newLine = document.createElement('li');
+  newLine.innerText = newObj.name + " costs $" + newObj.price;
+  container.appendChild(newLine);
+  // recalculate total
+  addTotal();
+}
+
+// delete last item from li on button click
+function deletePrev () {
+  var listArr = document.getElementById("listInDiv");
+  var last = list.length - 1;
+  listArr.removeChild(listArr.childNodes[last]);
+  // remove last item from list array
+  list.pop();
+  // recalculate total
+  addTotal();
+}
+
+// adds total to end of page
+function addTotal () {
+  var total = 0;
+  list.forEach(function (i) {
+    total += i['price'];
+  });
+  // add tax to total
+  total = total * 1.06;
+  // round to 2 decimal places
+  total = total.toFixed(2);
+
+  var totalDisplay = document.getElementById("total");
+  totalDisplay.innerText = "Your total is $" + total;
+}
+
+// initial call
+addTotal();
